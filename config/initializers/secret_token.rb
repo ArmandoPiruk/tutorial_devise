7e3138565066e6c978bcf39df116403886664c84
Armando Piruk <piruk7@gmail.com>
2012-02-21 21:52:58 -0300

    Inicicando tutorial para usar o Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TutoDevise::Application.config.secret_token = '635e75c3236155e1c99f9480fcffbed3ca2696e61a019f42c0b1dbb7d68d2f3355ed97cbca93a9cab98cc3b686d131bcb9a4e8ffa2117da90149ee3d42b8cbab'
